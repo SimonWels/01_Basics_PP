3c362d9453ad0c0f7c9dbbeed2f3361e2f6b7371
SimonWels <wels_freinsheim@web.de>
2024-07-01 10:25:26 +0000

    Deklaration + Wertzuweisung I

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,10 +1,26 @@
-// Zeilenkommentar | Str + #
+// Zeilenkommentar | Strg + #
 
 /**
  * 
  *  Block-Kommentar
  * 
  * 
+ * 
  */
+
+/***** Variablen 01 ******/
+
+// console.log("Hi");
+// console.log("hi");
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; // Wertzuweisung | assignment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log ("Hi, ich bin " + firstName + " " + familyName + "!"); // --> Hi, ich bin Max Mütze!
+
 
 
